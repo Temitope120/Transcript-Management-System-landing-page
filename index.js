4acab1c78c483b5d868f5dbbc1356f2a4d070796
Funminiyi Adedoyin <doyinfummi2012@gmail.com>
2022-09-13 08:41:35 +0100

    Clean up

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-const learnMoreBtn = document.getElementsByClassName('learn-more-btn');
-const cardText = document.querySelector('.card-text')
+// const learnMoreBtn = document.getElementsByClassName('learn-more-btn');
+// const cardText = document.querySelector('.card-text');
+// const moreText  = document.getElementsByClassName('moreText');
+// console.log('The hidden Text', moreText[0])
 
-for(let i = 0; i < learnMoreBtn.length; i++){
-    learnMoreBtn[i].addEventListener('click', (e)=>{
-        cardText.classList.toggle('show-more')
-    })
-    console.log(learnMoreBtn[i])
+
+
+ function showMore(item){
+    let textOne = document.getElementById('one');
+    let textTwo = document.getElementById('two');
+    let textThree = document.getElementById('three');
+
+    // cards
+    let cardOne = document.getElementById('card-one');
+    let cardTwo = document.getElementById('card-two');
+    let cardThree = document.getElementById('card-three');
+
+    if(item === "individual"){
+        textOne.classList.toggle('show-more');
+        cardOne.classList.toggle('h-auto')
+    }else if(item === "organization"){
+        textTwo.classList.toggle('show-more')
+        cardTwo.classList.toggle('h-auto')
+    }else{
+        textThree.classList.toggle('show-more');
+        cardThree.classList.toggle('h-auto')
+    }
+  
 }
